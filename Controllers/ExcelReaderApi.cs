aea702e9070f31436d5a5f13794df55614788f4d
Ali Shamas <127535679+zZSlinkerZz@users.noreply.github.com>
2023-09-01 11:40:58 +0300

    first commit

---
+++ Controllers/ExcelReaderApi.cs
@@ -0,0 +1,34 @@
+﻿namespace ExcelReaderApi;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class ExcelReaderApi : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly IExcelReaderService _excelReaderService;
+
+    public ExcelReaderApi(IConfiguration config, IExcelReaderService excelReaderService)
+        {
+            _excelReaderService = excelReaderService;
+            _config = config;
+        }
+
+    [HttpGet]
+    [Route("Data")]
+    public IActionResult ReadExcelFile()
+    {
+        var filePath = _config["ExcelSettings:FilePath"];
+        var people = _excelReaderService.ReadExcelData(filePath);
+        return Ok(people);
+
+    }
+
+    
+        
+    }
+
---
+++ Service.cs
@@ -0,0 +1,33 @@
+﻿using NPOI.XSSF.UserModel;
+
+public interface IExcelReaderService
+{
+    List<Person> ReadExcelData(string filePath);
+}
+
+public class ExcelReaderService : IExcelReaderService
+{
+    public List<Person> ReadExcelData(string filePath)
+    {
+        var people = new List<Person>();
+
+        using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        {
+            var workbook = new XSSFWorkbook(fs);
+            var sheet = workbook.GetSheetAt(0);
+
+            for (int i = 1; i <= sheet.LastRowNum; i++)
+            {
+                var row = sheet.GetRow(i);
+                var person = new Person
+                {
+                    Name = row.GetCell(0).StringCellValue,
+                    Age = (int)row.GetCell(1).NumericCellValue
+                };
+                people.Add(person);
+            }
+        }
+
+        return people;
+    }
+}
